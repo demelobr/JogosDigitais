2ccfdb2df56261b44a235c5998b0de93d6c9af39
Bruno Melo <demelobr10@gmail.com>
2024-09-12 11:52:43 -0300

    Ajustes finais na Fase 5.

---
+++ Assets/Scripts/Toten/Toten.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Toten : MonoBehaviour
+{
+
+    [SerializeField] private GameObject toten;
+    [SerializeField] private float totenHealth;
+
+    private bool isHitting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    public void OnHit()
+    {
+        totenHealth--;
+
+        if (totenHealth <= 0)
+        {
+            isHitting = true;
+            SceneManager.LoadScene("YouWin");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Axe") && !isHitting)
+        {
+            OnHit();
+        }
+    }
+
+}
